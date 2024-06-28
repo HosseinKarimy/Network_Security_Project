591de4152764f167f0c0a163b0f27414f4e43949
hossein karimy <hosseinkm01@gmail.com>
2024-06-28 12:07:51 +0330

    Basic Models

---
+++ Models/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class ContactModel
+{
+    [Key]
+    public string Id { get; set; }
+    
+    public string EncryptedName { get; set; }
+    public string EncryptedNumber { get; set; }
+    public string Sign { get; set; }
+}
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -5,5 +5,5 @@ namespace Models;
 public class UserModel
 {
     public string Username { get; set; }
-    public string SaltedHashedPassword { get; set; }
+    public string Password { get; set; }
 }
