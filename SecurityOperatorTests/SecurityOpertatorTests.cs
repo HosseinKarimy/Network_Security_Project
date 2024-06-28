475d3109f364c59fd390a0418a12d528d614b7b3
hossein karimy <hosseinkm01@gmail.com>
2024-06-28 12:06:05 +0330

    Security Operator Test

---
+++ SecurityOperatorTests/SecurityOpertatorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SecurityOperations.Tests
+{
+    [TestClass()]
+    public class SecurityOpertatorTests
+    {        
+
+        [TestMethod()]
+        public void EncrypteTest()
+        {
+            var SecOp = new SecurityOperator();
+
+            var plainText = "hello world";
+            var key = "myKey";
+
+            var cypherText = SecOp.Encryptor(plainText,key);
+            var decryptedText = SecOp.Decryptor(cypherText, key);
+
+            Assert.AreEqual(plainText, decryptedText);
+        }
+    }
+}
